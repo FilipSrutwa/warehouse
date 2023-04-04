de77599c96883c5fa7c5d49ffc205fde150ae057
FilipSrutwa <karok344@gmail.com>
2023-04-04 12:22:27 +0200

    Update topBottom.php

--- app/Views/layouts/topBottom.php
+++ app/Views/layouts/topBottom.php
@@ -31,7 +31,7 @@ if (session_status() === PHP_SESSION_NONE)
                     <a class="nav-link" href="/ManageContractors">Zarządzaj dostawcami</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="SalesHistory">Historia sprzedaży</a>
+                    <a class="nav-link" href="/SalesHistory">Historia sprzedaży</a>
                 </li>';
             </ul>
             <ul class="navbar-nav ">
