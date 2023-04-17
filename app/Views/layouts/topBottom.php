b85716c59774bb3765e92678ab18579693f8f0e1
FilipSrutwa <karok344@gmail.com>
2023-04-17 20:12:33 +0200

    Update topBottom.php

--- app/Views/layouts/topBottom.php
+++ app/Views/layouts/topBottom.php
@@ -45,7 +45,7 @@ if (session_status() === PHP_SESSION_NONE)
         <?= $this->renderSection('content') ?>
     </div>
     <div class="footer align-items-center justify-content-center bg-dark text-light text-center fixed-bottom" style="height: 5vh;">
-        <p style="font-size: 2.5vh;">Byliśmy tu: Filip Śrutwa i Riznychuk Valerii</p>
+        <p style="font-size: 2.5vh;">Warehouse system</p>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
